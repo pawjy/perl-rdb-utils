9b51c413500cb41e5f1bbc592602a04d92ca864b
Wakaba <wakaba@lemon>
2012-03-20 00:28:41 +0900

    Oops, timeout unintentionally disappeared during merge

--- lib/Test/MySQL/CreateDatabase.pm
+++ lib/Test/MySQL/CreateDatabase.pm
@@ -21,7 +21,10 @@ sub mysqld () {
         Test::mysqld->new(
             mysqld => $ENV{MYSQLD} || Test::mysqld::_find_program(qw/mysqld bin libexec sbin/),
             mysql_install_db => $ENV{MYSQL_INSTALL_DB} || Test::mysqld::_find_program(qw/mysql_install_db bin scripts/) . ($^O eq 'darwin' ? '' : ' '),
-            my_cnf => { 'skip-networking' => '' },
+            my_cnf => {
+                'skip-networking' => '',
+                'innodb_lock_wait_timeout' => 2,
+            },
         );
     } or BAIL_OUT($Test::mysqld::errstr);
     warn "done.\n" if $DEBUG;
